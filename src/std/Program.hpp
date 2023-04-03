8ca37fef7ca02c9293661ff670e248a4337f1e6d
pixelomer <37103802+pixelomer@users.noreply.github.com>
2023-04-03 20:21:31 +0300

    Make all methods of SmallBasic::Program static

--- src/std/Program.hpp
+++ src/std/Program.hpp
@@ -6,19 +6,19 @@
 namespace SmallBasic {
 	class Program {
 	public:
-		Number _GetArgumentCount() {
+		static Mixed _GetArgumentCount() {
 			throw std::runtime_error("Not implemented");
 		}
-		Mixed _GetDirectory() {
+		static Mixed _GetDirectory() {
 			return Mixed(std::filesystem::current_path());
 		}
-		void End() {
+		static void End() {
 			exit(0);
 		}
-		String GetArgument(Number index) {
+		static Mixed GetArgument(Number index) {
 			throw std::runtime_error("Not implemented");
 		}
-		void Delay(Number milliseconds) {
+		static void Delay(Number milliseconds) {
 			std::this_thread::sleep_for(std::chrono::milliseconds(
 				(long long)milliseconds));
 		}
