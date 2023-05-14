7264cb4299374ab8d517a25a45bdda9fe07930d0
pixelomer <37103802+pixelomer@users.noreply.github.com>
2023-05-14 15:14:44 +0300

    Fix Text::GetSubText()

--- src/std/std/Text.hpp
+++ src/std/std/Text.hpp
@@ -26,7 +26,7 @@ namespace SmallBasic {
 			}
 			static Mixed GetSubText(String const& str, Number start, Number length) {
 				start -= 1;
-				if (start <= 0) return L"";
+				if (start < 0) return L"";
 				return str.substr(start, std::max((int)std::min(str.size() - start, length), 0));
 			}
 			static Mixed GetSubTextToEnd(String const& str, Number start) {
