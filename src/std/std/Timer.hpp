c5c0c7d16bc7e95af40f302da4536733119b1635
pixelomer <37103802+pixelomer@users.noreply.github.com>
2023-05-14 23:45:07 +0300

    Remove all timer (broken) functionality

--- src/std/std/Timer.hpp
+++ src/std/std/Timer.hpp
@@ -3,8 +3,6 @@
 
 #include "../common/Mixed.hpp"
 #include "../common/Updatable.hpp"
-#include <chrono>
-#include <cmath>
 
 namespace SmallBasic {
 	namespace Platform {
@@ -15,34 +13,23 @@ namespace SmallBasic {
 		private:
 			friend class Platform::RunLoop;
 			static bool _active;
-			static Updatable<Number> _interval;
-			static std::chrono::time_point<std::chrono::steady_clock,
-				std::chrono::steady_clock::duration> _timerStart;
 			static void (*_onTick)();
 		public:
 			static void _SetTick(void(*onTick)()) {
 				_onTick = onTick;
 			}
 			static Mixed _GetInterval() {
-				return _interval.get();
+				return 1000.L;
 			}
 			static void _SetInterval(Number milliseconds) {
-				_timerStart = std::chrono::high_resolution_clock::now();
-				_interval = milliseconds;
 			}
 			static void Pause() {
-				_active = false;
 			}
 			static void Resume() {
-				_timerStart = std::chrono::high_resolution_clock::now();
-				_active = true;
 			}
 		};
 
-		bool Timer::_active = true;
-		Updatable<Number> Timer::_interval = 1000;
-		std::chrono::time_point<std::chrono::steady_clock,
-			std::chrono::steady_clock::duration> Timer::_timerStart = {};
+		bool Timer::_active = false;
 		void (*Timer::_onTick)() = NULL;
 	}
 }
