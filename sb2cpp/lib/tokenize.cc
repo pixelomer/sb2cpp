ffc34d2b851f2ee02d1f7fd526118b220205e3b5
pixelomer <37103802+pixelomer@users.noreply.github.com>
2024-01-26 15:23:56 +0300

    Don't use wchar_t functions

--- sb2cpp/lib/tokenize.cc
+++ sb2cpp/lib/tokenize.cc
@@ -16,8 +16,8 @@ std::vector<std::string> tokenize(std::string const& source) {
     const std::string special = "()[]<>+-/*=.',:";
     size_t len = source.length();
     for (int i=0; i<len; i++) {
-        wchar_t c = source[i];
-        if (iswspace(c)) {
+        char c = source[i];
+        if (std::isspace(c)) {
             // whitespace
             if (c == L'\n') {
                 if (last_token != "\n") {
@@ -49,11 +49,11 @@ std::vector<std::string> tokenize(std::string const& source) {
                 }
             }
         }
-        else if (isdigit((int)c)) {
+        else if (std::isdigit((int)c)) {
             // number
             bool parsed_dot = false;
             do { token += source[i++]; }
-            while (i < len && (isdigit((int)(c = source[i]))
+            while (i < len && (std::isdigit((int)(c = source[i]))
                 || (!parsed_dot && (parsed_dot = (c == L'.')))));
             i--;
         }
@@ -75,7 +75,7 @@ std::vector<std::string> tokenize(std::string const& source) {
                 token += source[i++];
             }
             while (i < len && (special.find(source[i]) == std::string::npos)
-                && !iswspace(source[i]));
+                && !std::isspace(source[i]));
             i--;
         }
         tokens.push_back(token);
