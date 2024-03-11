e2e6b124c5183b644ca3ec2f4bf605b52340bcdb
pixelomer <37103802+pixelomer@users.noreply.github.com>
2024-03-11 15:59:59 +0300

    Fix Text.GetSubText() and Text.GetSubTextToEnd()

--- sb2cpp/stdlib/Text.hpp
+++ sb2cpp/stdlib/Text.hpp
@@ -43,10 +43,16 @@ SB_METHOD_2(StartsWith) (std::string const& text, std::string const& subtext) {
 }
 
 SB_METHOD_3(GetSubText) (std::string const& text, long pos, long len) {
-    return text.substr(pos-1, len);
+    if (pos-1 >= text.length()) {
+        return Obj("");
+    }
+    return text.substr(pos-1, std::min(len, (long)(text.length()-pos+1)));
 }
 
 SB_METHOD_2(GetSubTextToEnd) (std::string const& text, long pos) {
+    if (pos-1 >= text.length()) {
+        return Obj("");
+    }
     return text.substr(pos-1);
 }
 
